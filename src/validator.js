48644edb120e064af4c911ed6111d267be9d0324
nemnogokartavyy <iikorzun@yandex.by>
2026-03-12 01:52:29 +0300

    create validator.js

--- src/validator.js
+++ src/validator.js
@@ -1,1 +1,27 @@
 // data validator
+
+export function validate(car) {
+    const requiredFields = [
+        "slug",
+        "brand",
+        "model",
+        "year",
+        "engine",
+        "drive",
+        "transmission",
+        "body",
+        "class",
+        "seats",
+        "delivery",
+        "insurance",
+        "price_day",
+        "mileage_per_day",
+        "extra-km-price"
+    ];
+
+    for (const field of requiredFields) {
+        if (!car[field] && car[field] !== 0) {
+            throw new Error(`Car missing required field: ${field}`);
+        }
+    }
+}
