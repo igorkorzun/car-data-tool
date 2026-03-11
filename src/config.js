2d94f02efcb1fd8ca435ae49073b4af93800bdd1
nemnogokartavyy <iikorzun@yandex.by>
2026-03-12 01:01:58 +0300

    create config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,13 @@
 // path & settings
+
+import path from "path";
+
+export const CSV_PATH = "/var/www/data/cars.csv";
+export const JSON_PATH = "/var/www/data/cars.json";
+export const UPLOADS_PATH = "/var/www/uploads";
+export const CARS_PATH = "/var/www/data/cars"; // images
+
+export const IMAGE_SIZES = {
+  cover: 1600,
+  gallery: 1200,
+};
