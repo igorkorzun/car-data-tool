73b501610daebe4d801f6d5208f59f077ad7f759
nemnogokartavyy <iikorzun@yandex.by>
2026-03-12 01:18:58 +0300

    create csvParser.js

--- src/csvParser.js
+++ src/csvParser.js
@@ -1,1 +1,20 @@
 // reading CSV
+
+import fs from "fs";
+import Papa from "papaparse";
+import { CSV_PATH } from "./config.js";
+
+export function parseCSV() {
+    // const file = fs.readFileSync(CSV_PATH, "utf8");
+    const file = fs.readFileSync("./cars.csv", "utf8"); // test
+    console.log("file: ", file); // test
+    const result = Papa.parse(file, {
+        header: true,
+        skipEmptyLines: true,
+    });
+    console.log("result: ", result); // test
+    console.log("result.data: ", result.data); // test
+    return result.data;
+}
+
+console.log(parseCSV()); // test
